c3dfcaabfd2c0ece9eb5985c06c9377da20aaea2
isabelajs <isabelajimenezs96@gmail.com>
2021-03-02 18:12:08 -0500

    arregle el ultimo elemento del menu y el componente de unidades

--- src/componentes/mainPage/product.js
+++ src/componentes/mainPage/product.js
@@ -1,15 +1,22 @@
 function renderProduct (producto){
+  //creo el elemento html 
+    const product = document.createElement('div');
+    product.classList.add('c-Product');
+  //le agrego información
     const view = `  
     <div class="c-Product__info">
         <img class="c-Product__img" src="${producto.imgUrl}" alt="">
         <div class="c-Product__name">${producto.name}</div>
         <div class="c-Product__price">$ ${producto.price}</div>
     </div>
-    <div class="c-Product__button c-button">PAGAR</div>`
-
-    const product = document.createElement('div');
-    product.classList.add('c-Product');
+    <div class="c-Product__buttonPay c-button">PAGAR</div>`
     product.innerHTML = view;
+
+    let buttonPay = product.querySelector('.c-Product__buttonPay')
+
+    buttonPay.addEventListener('click',()=>{
+      console.log('hello');
+    })
 
     return product
 }
@@ -25,3 +32,13 @@ export{renderProduct}
       <div class="c-Product__button c-button">PAGAR</div>
 </div>  */}
 
+{/* <div class="c-Product__buttonUnits">
+      
+      <div class="buttonUnits plus">
+      </div>
+
+      <p class="c-Product__buttonUnitsText">1 Und</p>
+
+      <div class=" buttonUnits minus"></div>
+
+   </div> */}
